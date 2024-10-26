e6eb652c2e28ddd18bb76ca2ebfee1411490cb7a
Ana Zambrana <anazambrana668@gmail.com>
2024-10-25 21:58:38 -0300

    Se realizaron correciones en el codigo

--- main.js
+++ main.js
@@ -9,14 +9,22 @@ for (let contador = 0; contador<listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador]; 
     const instrumento = tecla.classList[1]; 
     console.log(contador); 
+
     const idAudio = `#sonido_${instrumento}`; 
     console.log(idAudio); 
 
     tecla.onclick = function (){
         playSonido(idAudio); 
     }; 
-    tecla.onkeydown = function (){
-        tecla.classList.add('activa'); 
+    tecla.onkeydown = function(evento){
+        
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa'); 
+        }
+        console.log(evento.code === 'Space' || evento.code === 'Enter')
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa'); 
     }
     
 }
